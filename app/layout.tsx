1c2835ea362d50853ad037493fc3d6d046742bde
Regie <114920732+RegieSanJuan@users.noreply.github.com>
2026-03-18 13:50:31 +0800

    new change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,21 +10,9 @@ export const metadata: Metadata = {
   title: 'Secret Game',
   description: 'Laruin mo to beh',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/buti%20nalang%20na%20solve%20mo%20beh.png',
+    shortcut: '/buti%20nalang%20na%20solve%20mo%20beh.png',
+    apple: '/buti%20nalang%20na%20solve%20mo%20beh.png',
   },
 }
 
